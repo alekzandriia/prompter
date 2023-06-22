261d725b0ea0d727734c3580891f14ac6f504709
Alekzandriia <alekzoreillyz@me.com>
2023-06-22 18:38:27 -0400

    Add words

--- prompt.js
+++ prompt.js
@@ -1,10 +1,10 @@
 // Word Bank
 const words = {
   adjectives: [
-    "hollow", "silly", "dark", "ghostly", "arid", "tumultuous"
+    "hollow", "silly", "dark", "ghostly", "arid", "expensive", "tumultuous", "bald", "careful", "gentle", "glamorous", "loaded", "sarcastic", "uninterested", "impromptu", "abandoned", "bewitched", "disfigured", "emotional", "filthy", "edible", "smelly","unethical", "capricious", "precocious", "questionable", "secret", "gigantic"
   ],
   nouns: [
-    "clock", "midnight", "wallet", "opportunity", "playlist"
+    "clock", "midnight", "wallet", "opportunity", "playlist", "vacation", "examination", "painting", "virtual reality", "city", "girl", "man", "woman", "boy", "grandparent", "boss", "newspaper", "skyscraper", "farm", "forest", "goldfish", "job offer", "drug", "officer", "pilot", "cell phone", "diary", "flight", "date", "tournament", "compass", "hideout", "door", "room", "interview", "bacteria"
   ],
 }
 
